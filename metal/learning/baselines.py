fbf29427db0a33c7538e79cbd3eceb06fd19b94f
Braden Hancock <hancock.braden@gmail.com>
2018-05-25 11:51:12 -0700

    Add MajorityClassVoter to baselines

--- metal/learning/baselines.py
+++ metal/learning/baselines.py
@@ -23,7 +23,7 @@ class RandomVoter(LabelModelBase):
 class MajorityVoter(LabelModelBase):
     """
     A class that treats every task independently, placing all probability on 
-    the label(s) with the majority vote of all non-abstaining LFs for that task.
+    the majority label from all non-abstaining LFs for that task.
 
     Note that in the case of ties, non-integer probabilities are possible.
     """
@@ -45,6 +45,36 @@ class MajorityVoter(LabelModelBase):
         Y_ts /= Y_ts.sum(axis=1).reshape(-1, 1)
         return torch.tensor(Y_ts, dtype=torch.float)
 
-class StructuredMajorityVoter(MajorityVoter):
-    def __init__(self):
-        raise NotImplementedError
+
+class MajorityClassVoter(LabelModelBase):
+    """
+    A class that treats every task independently, placing all probability on
+    the majority class based on class balance (and ignoring the label matrix).
+
+    Note that in the case of ties, non-integer probabilities are possible.
+    """
+    def __init__(self, L_train, balances, **kwargs):
+        """
+        Args:
+            balances: A list of T lists or np.arrays that each sum to 1, 
+                corresponding to the (possibly estimated) class balances for 
+                each task.
+        """
+        super().__init__(L_train, **kwargs)
+        assert(isinstance(balances, list))
+        assert(len(balances) == self.T)
+        assert(all(isinstance(x, np.ndarray) for x in balances))
+        self.balances = balances
+
+
+    def train(self):
+        "MajorityVoter does not need to train. Skipping..."
+
+    def predict_proba(self, L, t):
+        N = L.shape[0]
+        Y_ts = np.zeros((N, self.K_t[t]))
+        max_classes = np.where(self.balances[t] == max(self.balances[t]))
+        for c in max_classes:
+            Y_ts[:, c] = 1.0
+        Y_ts /= Y_ts.sum(axis=1).reshape(-1, 1)
+        return torch.tensor(Y_ts, dtype=torch.float)
