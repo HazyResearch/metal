7a47b85d9225a8391862745daebb04aae5da999e
Braden Hancock <hancock.braden@gmail.com>
2018-05-26 03:58:24 -0700

    Upgrade predict_proba methods from LabelModel to Classifier

--- metal/classifier.py
+++ metal/classifier.py
@@ -1,17 +1,52 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
 from metal.metrics import metric_score, confusion_matrix
 
-class Classifier(object):
+class Classifier(nn.Module):
     """Simple abstract base class for a probabilistic classifier."""
 
-    def __init__(self, cardinality=2, name=None):
-        self.name = name or self.__class__.__name__
-        self.cardinality = cardinality
+    def __init__(self):
+        super().__init__()
 
-    def train(self, X, **kwargs):
+    def train(self, X, Y, **kwargs):
         raise NotImplementedError
 
-    def predict(self, X, **kwargs):
+    def predict_proba(self, X, t=0, **kwargs):
+        """Returns an [N, K_t] tensor of soft (float) predictions for task t."""
         raise NotImplementedError
+
+    def predict(self, X, t=0, break_ties='random'):
+        """Returns an N-dim tensor of hard (int) predictions for task t."""
+        Y_ts = self.predict_proba(X, t=t).numpy()
+
+        N, k = Y_ts.shape
+        Y_th = np.zeros(N)
+        diffs = np.abs(Y_ts - Y_ts.max(axis=1).reshape(-1, 1))
+
+        TOL = 1e-5
+        for i in range(N):
+            max_idxs = np.where(diffs[i, :] < TOL)[0]
+            if len(max_idxs) == 1:
+                Y_th[i] = max_idxs[0] + 1
+            # Deal with 'tie votes' according to the specified policy
+            elif break_ties == 'random':
+                Y_th[i] = np.random.choice(max_idxs) + 1
+            elif break_ties == 'abstain':
+                Y_th[i] = 0
+            else:
+                ValueError(f'break_ties={break_ties} policy not recognized.')
+        
+        return torch.tensor(Y_th, dtype=torch.short)
+
+    def predict_tasks_proba(self, X):
+        """Returns a list of T [N, K_t] tensors of soft (float) predictions."""
+        return [self.predict_proba(X, t=t) for t in range(self.T)]
+
+    def predict_tasks(self, X, break_ties='random'):
+        """Returns a list of T [N, K_t] tensors of hard (int) predictions."""
+        return [self.predict(X, t=t, break_ties=break_ties) for t in range(self.T)]
 
     def score(self, X, Y, metric='accuracy', verbose=True, **kwargs):
         Y_p = self.predict(X, **kwargs)
--- metal/label_model/label_model.py
+++ metal/label_model/label_model.py
@@ -51,40 +51,9 @@ class LabelModelBase(Classifier):
 
         self.K_t = [len(labels) for labels in self.label_map]
 
-    def predict_tasks_proba(self, L):
-        """Returns a list of T [N, K_t] tensors of soft (float) predictions."""
-        return [self.predict_proba(L, t=t) for t in range(self.T)]
-
-    def predict_tasks(self, L, break_ties='random'):
-        """Returns a list of T [N, K_t] tensors of hard (int) predictions."""
-        return [self.predict(L, t=t, break_ties=break_ties) for t in range(self.T)]
-
     def predict_proba(self, L, t=0):
         """Returns an [N, K_t] tensor of soft (float) predictions for task t."""
         raise NotImplementedError
-
-    def predict(self, L, t=0, break_ties='random'):
-        """Returns an N-dim tensor of hard (int) predictions for task t."""
-        Y_ts = self.predict_proba(L, t=t).numpy()
-
-        N, k = Y_ts.shape
-        Y_th = np.zeros(N)
-        diffs = np.abs(Y_ts - Y_ts.max(axis=1).reshape((-1, 1)))
-
-        TOL = 1e-5
-        for i in range(N):
-            max_idxs = np.where(diffs[i, :] < TOL)[0]
-            if len(max_idxs) == 1:
-                Y_th[i] = max_idxs[0] + 1
-            # Deal with 'tie votes' according to the specified policy
-            elif break_ties == 'random':
-                Y_th[i] = np.random.choice(max_idxs) + 1
-            elif break_ties == 'abstain':
-                Y_th[i] = 0
-            else:
-                ValueError(f'break_ties={break_ties} policy not recognized.')
-        
-        return torch.tensor(Y_th, dtype=torch.short)
 
 
 class LabelModel(LabelModelBase):
