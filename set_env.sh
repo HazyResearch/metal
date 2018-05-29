acf25a6dbb44ab066d168a07b35c9e5de2d011cc
Braden Hancock <hancock.braden@gmail.com>
2018-05-29 12:21:36 -0700

    Add warning to set_env and jupyter instructions to README

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,5 @@
+echo "USE THIS WITH CAUTION!"
+echo "set_env.sh will not be a part of the final repo"
 export METALHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "MeTaL home directory: $METALHOME"
 export PYTHONPATH="$PYTHONPATH:$METALHOME"
