3a9ed920e63d2d9e9929d1e60f8e91f033aa6665
Braden Hancock <hancock.braden@gmail.com>
2018-05-29 12:21:17 -0700

    Add assert statements to test_label_model and add other baselines

--- tests/test_label_model.py
+++ tests/test_label_model.py
@@ -1,19 +1,25 @@
 import sys
-sys.path.append("../metal")
-
 import unittest
 
 import numpy as np
 import torch
 
-from synthetics.generate import generate_single_task_unipolar
+sys.path.append("../metal")
 from metal.label_model.label_model import LabelModel
-from metal.label_model.baselines import MajorityLabelVoter
+from metal.label_model.baselines import (
+    RandomVoter,
+    MajorityClassVoter,
+    MajorityLabelVoter,
+)
+
+sys.path.append("../synthetics")
+from synthetics.generate import generate_single_task_unipolar
 
 
 class LabelModelTest(unittest.TestCase):
 
-    def test_single_task_basic(self):
+    @classmethod
+    def setUpClass(cls):
         # Set seed
         np.random.seed(1)
 
@@ -22,20 +28,38 @@ class LabelModelTest(unittest.TestCase):
         L, Y, metadata = generate_single_task_unipolar(N, M, acc=[0.4, 0.8], 
             rec=[0.5])
 
-        # Initialize label model
+        cls.single = (L, Y, metadata)
+
+    def test_single_random(self):
+        L, Y, _ = self.single
+        model = RandomVoter()
+        model.train(L)
+        score = model.score(L, Y, verbose=False)
+        self.assertAlmostEqual(score, 0.499, places=2)
+
+    def test_single_mc(self):
+        L, Y, metadata = self.single
+        balances = [metadata['class_balance']]
+        model = MajorityClassVoter()
+        model.train(L, balances)
+        score = model.score(L, Y, verbose=False)
+        self.assertAlmostEqual(score, 0.496, places=2)
+
+    def test_single_mv(self):
+        L, Y, _ = self.single
+        model = MajorityLabelVoter()
+        model.train(L)
+        score = model.score(L, Y, verbose=False)
+        self.assertAlmostEqual(score, 0.787, places=2)
+
+    def test_single_lm(self):
+        L, Y, metadata = self.single
         model = LabelModel()
-
-        # Train label model
-        model.train(L, accs=metadata['accs'])
-
-        # Test label model
-        model.score([L], [Y])
-
-        # Compare with MV
-        print("\nMajority vote baseline:")
-        mv = MajorityLabelVoter()
-        mv.train([L])
-        mv.score([L], [Y])
+        model.train(L, accs=metadata['accs'], verbose=False)
+        score = model.score(L, Y, verbose=False)
+        accs_score = model.get_accs_score(metadata['accs'])
+        self.assertAlmostEqual(score, 0.826, places=2)
+        self.assertLess(accs_score, 0.001)
 
 
 if __name__ == '__main__':
