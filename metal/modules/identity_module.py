d9c90889d13d63e1d903227e3f60ab10e8d88e52
Braden Hancock <hancock.braden@gmail.com>
2018-05-31 20:37:41 -0700

    Create IdentityModule

--- metal/modules/identity_module.py
+++ metal/modules/identity_module.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 from metal.modules.base_module import InputModule
 
 class IdentityModule(InputModule):
-    """A generic nn.Module class with a method for getting the output dim."""
-    def __init__(self):
-        super().__init__() 
+    """A default identity input layer that simply passes the input through."""
+    def __init__(self, output_dim):
+        super().__init__()
+        self.output_dim = output_dim
    
     def get_output_dim(self):
-        raise NotImplementedError()
+        return self.output_dim
