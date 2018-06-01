50a911b0a43b8be21287df5b1d2cfd78144843d3
Braden Hancock <hancock.braden@gmail.com>
2018-05-31 17:14:28 -0700

    Improve readability of set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 echo "USE THIS WITH CAUTION!"
 echo "set_env.sh will not be a part of the final repo"
+
 export METALHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "MeTaL home directory: $METALHOME"
+
 export PYTHONPATH="$PYTHONPATH:$METALHOME"
 echo "Using PYTHONPATH=${PYTHONPATH}"
 export PATH="$PATH:$METALHOME"
 echo "Environment variables set!"
-
-# export CUDA_VISIBLE_DEVICES=0
