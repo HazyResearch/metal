fe58e8914255bf9a5eeb72a814725589ed234fce
Braden Hancock <hancock.braden@gmail.com>
2018-06-05 13:43:22 -0700

    Add test_end_model tests

--- tests/test_end_model.py
+++ tests/test_end_model.py
@@ -6,6 +6,7 @@ import torch
 
 sys.path.append("../metal")
 from metal.end_model import EndModel
+from metal.structs import TaskGraph
 from metal.utils import hard_to_soft
 
 class EndModelTest(unittest.TestCase):
@@ -34,19 +35,54 @@ class EndModelTest(unittest.TestCase):
 
         cls.Y_train = [hard_to_soft(Y_t, k=2) for Y_t in cls.Y_train]
 
-    def test_single_task(self):
-        em = EndModel(seed=1)
+    def test_singletask(self):
+        em = EndModel(seed=1, verbose=False)
         Y_train = self.Y_train[0]
         Y_dev = self.Y_dev[0]
         Y_test = self.Y_test[0]
         em.train(self.X_train, Y_train, self.X_dev, Y_dev, 
+            verbose=False,
             n_epochs=10,
             dropout=0.0,
             layer_output_dims=[2, 4, 2],
         )
-        score = em.score(self.X_test, Y_test, reduce='mean')
+        score = em.score(self.X_test, Y_test, verbose=False)
+        self.assertEqual(score, 0.8)
+
+    def test_multitask_top(self):
+        label_map=[[1,2],[1,2]]
+        em = EndModel(
+            label_map=label_map, 
+            seed=1,
+            verbose=False,
+            dropout=0.0,
+            layer_output_dims=[2,4,2],
+            head_layers='top',
+        )
+        em.train(self.X_train, self.Y_train, self.X_dev, self.Y_dev,
+            verbose=False,
+            n_epochs=10,
+        )
+        score = em.score(self.X_test, self.Y_test, reduce='mean', verbose=False)
         self.assertEqual(score, 0.74)
 
-
+    # def test_multitask_auto(self):
+        # label_map=[[1,2],[1,2]]
+        # edges = [(0,1)]
+        # cards = [2,2]
+        # tg = TaskGraph(edges, cards)
+        # em = EndModel(
+        #     label_map=label_map,
+        #     task_graph=tg,
+        #     seed=1,
+        #     dropout=0.0,
+        #     layer_output_dims=[2,4,2],
+        #     head_layers='auto')
+        # em.train(self.X_train, self.Y_train, self.X_dev, self.Y_dev,
+        #     verbose=False,
+        #     n_epochs=10,)
+        # score = em.score(self.X_test, self.Y_test, reduce='mean')
+        # self.assertEqual(score, ?)
+        
 if __name__ == '__main__':
     unittest.main()        
