a7ae887547becc01818cefd25c72411b6e4aa44d
ajratner <ajratner@gmail.com>
2018-07-26 08:23:56 -0700

    New LabelModel that handles class-conditional + deps, from the notebook

--- metal/label_model/label_model.py
+++ metal/label_model/label_model.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 from scipy.sparse import issparse, csc_matrix
 import torch
@@ -16,7 +18,7 @@ from metal.utils import recursive_merge_dicts
 
 
 class LabelModel(Classifier):
-    def __init__(self, deps=[], **kwargs):
+    def __init__(self, p, deps=[], **kwargs):
         """
         Args:
             seed: int: Random state seed
@@ -24,256 +26,153 @@ class LabelModel(Classifier):
             kwargs: TBD
         """
         self.config = recursive_merge_dicts(lm_model_defaults, kwargs)
-        super().__init__(self.config['cardinality'], self.config['seed'])
+        super().__init__()
+
+        self.P = torch.diag(torch.from_numpy(p)).float() # Class balance matrix
+        self.k = len(p)
         self.deps = deps
 
-        # TODO: This is temporary, need to update to handle categorical!
-        if self.k > 2:
-            raise NotImplementedError("Cardinaltiy > 2 not implemented.")
-        
-        # TODO: Extend to handl LF deps (merge with other branch...)
-        if len(self.deps) > 0:
-            raise NotImplementedError("Dependency handling not implemented.")
-    
-    def _check_L(self, L, init=False):
-        """Check the format and content of the label tensor
-        
-        Args:
-            L: An [n, m] scipy.sparse matrix of labels
-            init: If True, initialize self.m; else set based on input L
-        """
-        # Accept single CSC-sparse matrix (for efficient column/LF slicing)
-        if not issparse(L):
-            raise Exception(f"L has type {type(L)}, but should be a"
-                "scipy.sparse matrix.")
-        if L.dtype != np.dtype(int):
-            raise Exception(f"L has type {L.dtype}, should be int.")
-        L = L.tocsc()
-
-        # Check or set number of labeling functions
-        # M should be the same for all tasks, since all LFs label all inputs
-        n, m = L.shape
-        self._check_or_set_attr('m', m, set_val=init)
-        return L    
-    
-    def _infer_polarity(self, L, j):
-        """Infer the polarity (labeled class) of LF j"""
-        assert(isinstance(L, csc_matrix))
-        vals = set(L.data[L.indptr[j]:L.indptr[j+1]])
-        if len(vals) > 1:
-            raise Exception(f"LF {j} is non-unipolar with values: {vals}.")
-        elif len(vals) == 0:
-            # If an LF doesn't label this task, set its polarity to 0 = abstain
-            return 0
-        else:
-            return list(vals)[0]
-    
-    def _compute_overlaps_matrix(self, L):
-        """Initializes the data structures for training task t"""
-        # Check to make sure that L_t is unipolar, and infer polarities
-        self.polarity = [self._infer_polarity(L, j) for j in range(self.m)]
-
-        # Next, we form the empirical overlaps matrix O
-        # In the unipolar categorical setting, this is just the empirical count
-        # of non-zero overlaps; whether these were agreements or disagreements
-        # is then just a function of the polarities p
-        n, m = L.shape
-        L_nz = L.copy()
-        L_nz.data[:] = 1
-        O = L_nz.T @ L_nz / n 
-        self.O = torch.from_numpy(O.todense()).double()
-    
-    def _init_params(self, mu_init, learn_class_balance, class_balance_init):
-        """Initialize the parameters for each LF on each task separately.
-        
-        Args:
-            mu_init: float: Initial value & prior value for parameter mu, where
-                mu is the rank-2 factorization of O. Here, one row represents
-                (e.g. for k=2): 
-                    $[P(\lambda_i=p_i|Y=1), P(\lambda_i=p_i|Y=2)]$
-                where $p_i \in {1,2}$ is the polarity of $\lambda_i$.
-            learn_class_balance: bool: If true, learn the class balance
-                parameters $P(Y=y_k)$
-            class_balance_init: array(float) or None: If None, set to be the
-                uniform distribution across the classes. If 
-                `learn_class_balance=True`, then this is the initial value & 
-                prior value for the class balance; else, this is fixed as the
-                class balance.
-        """
-        self.mu_init = mu_init
-        self.mu = nn.Parameter(torch.randn(self.m, 2).double())
-
-        # Default to uniform class_balance_init
-        if class_balance_init is None:
-            self.class_balance_init = torch.ones(self.k) / self.k
-        else:
-            self.class_balance_init = self._to_torch(class_balance_init)
-        self.class_balance_init = self.class_balance_init.double()
-
-        # Class balance- can be fixed or learnable
-        if learn_class_balance:
-            self.class_balance = nn.Parameter(self.class_balance_init.double())
-        else:
-            self.class_balance = self.class_balance_init
-
-        # Initialize mask
-        self.mask = torch.ones(self.m, self.m).byte()
-        for i in range(self.m):
-            for j in range(self.m):
-                if i == j or (i,j) in self.deps or (j,i) in self.deps:
-                    self.mask[i,j] = 0
-    
-    def loss(self, l2=0.0):
-        """Returns the loss.
-
-        Note: A *centered* L2 loss term is incorporated here.
-        The L2 term is centered around the self.mu_init property, which thus
-        also serves as the value of a prior on the mus.
-        """
-        P = torch.diag(self.class_balance)
-
-        # Main masked rank-two matrix factorization objective
-        loss_1 = torch.norm((self.O - self.mu @ P @ self.mu.t())[self.mask] )**2
-
-        # Constraint that the entries of mu for each LF sum to the total
-        # probability of emitting a label (the diagonal entry of O)
-        loss_2 = torch.norm(torch.sum(self.mu @ P, 1) - torch.diag(self.O))**2
-        
-        # Constraint that the class balance parameters sum to one
-        loss_3 = torch.norm(torch.sum(self.class_balance) - 1)**2
-
-        # L2 regularization, centered around mu_init
-        loss_L2 = torch.norm(self.mu - self.mu_init)**2
-        return loss_1 + loss_2 + loss_3 + l2 * loss_L2
+        # Whether to take the simple conditionally independent approach, or the
+        # "inverse form" approach for handling dependencies
+        # This flag allows us to eg test the latter even with no deps present
+        self.inv_form = (len(self.deps) > 0)
     
     def config_set(self, update_dict):
         """Updates self.config with the values in a given update dictionary"""
         self.config = recursive_merge_dicts(self.config, update_dict)
-
-    def _betas(self):
-        """Returns the estimated beta (labeling propensity) parameters.
-        Note: As an internal function, returns torch.Tensor.
-        """
-        return torch.sum(self.mu.detach(), 1)
     
-    def _alphas(self):
-        """Returns the estimated alpha (accuracy) parameters.
-        Note: As an internal function, returns torch.Tensor.
-        """
-        alphas = self.mu.detach() / self._betas().repeat(2,1).t()
+    def _generate_O(self, L):
+        """Form the overlaps matrix, which is just all the different observed
+        combinations of values of pairs of sources
 
-        # Swap elements in each row so that column 1 corresponds to the polarity
-        # of the LF---i.e. represents P(\lf_i = p_i | Y = p_i)---and column 2
-        # is then P(\lf_i = p_i | Y != p_i)
-        # TODO: Update this to support categorical
-        for j in range(self.m):
-            if self.polarity[j] != 1:
-                row = alphas[j].numpy().copy()
-                alphas[j,0] = row[1]
-                alphas[j,1] = row[0]
+        Note that we only include the first (k-1) values of each source,
+        otherwise the model not minimal --> leads to singular matrix
+        """
+        self.n, self.m = L.shape
+        self.d = self.m * (self.k-1)
+        self.O = torch.zeros(self.d, self.d).float()
+
+        # TODO: Generate this matrix in a more efficient way?
+        for (y1, y2) in product(range(self.k-1), range(self.k-1)):
+            O_i = np.where(L == y1, 1, 0).T @ np.where(L == y2, 1, 0) / self.n
+            for j1 in range(self.m):
+                for j2 in range(self.m):
+                    self.O[j1*(self.k-1) + y1, j2*(self.k-1) + y2] = O_i[j1,j2]
+    
+    def _generate_O_inv(self, L):
+        """Form the *inverse* overlaps matrix"""
+        self._generate_O(L)
+        self.O_inv = torch.from_numpy(np.linalg.inv(self.O.numpy())).float()
+    
+    def _init_params(self):
+        """Initialize the learned params"""
+        self.mu = nn.Parameter(torch.randn(self.d, self.k)).float()
+        if self.inv_form:
+            self.Z = nn.Parameter(torch.randn(self.d, self.k)).float()
+
+        # Initialize the mask for the masked matrix approximation step; masks 
+        # out the block diagonal of O and any dependencies
+        km = self.k - 1
+        self.mask = torch.ones(self.d, self.d).byte()
+        for (i, j) in product(range(self.m), range(self.m)):
+            if i == j or (i,j) in self.deps or (j,i) in self.deps:
+                self.mask[i*km:(i+1)*km, j*km:(j+1)*km] = 0
+
+    def loss_inv_Z(self):
+        return torch.norm((self.O_inv + self.Z @ self.Z.t())[self.mask])**2
+    
+    def get_Q(self):
+        """Get the model's estimate of Q = \mu P \mu^T
         
-        # Need to break symmetry in the columns by assuming the more 
-        # net-accurate one is correct
-        # TODO: Update this to support categorical
-        if torch.sum(1-alphas[:,0]) > torch.sum(alphas[:,0]):
-            alphas_col = alphas[:,1]
+        We can then separately extract \mu subject to additional constraints,
+        e.g. \mu P 1 = diag(O).
+        """
+        Z = self.Z.detach().clone().numpy()
+        O = self.O.numpy()
+        I_k = np.eye(self.k)
+        return O @ Z @ np.linalg.inv(I_k + Z.T @ O @ Z) @ Z.T @ O
+
+    def loss_inv_mu(self):
+        loss_1 = torch.norm(self.Q - self.mu @ self.P @ self.mu.t())**2
+        loss_2 = torch.norm(
+            torch.sum(self.mu @ self.P, 1) - torch.diag(self.O))**2
+        return loss_1 + loss_2
+    
+    def loss_mu(self):
+        loss_1 = torch.norm(
+            (self.O - self.mu @ self.P @ self.mu.t())[self.mask])**2
+        loss_2 = torch.norm(
+            torch.sum(self.mu @ self.P, 1) - torch.diag(self.O))**2
+        return loss_1 + loss_2
+    
+    def train(self, L, **kwargs):
+        """Train the model (i.e. estimate mu) in one of two ways, depending on
+        whether source dependencies are provided or not:
+        
+        (1) No dependencies (conditionally independent sources): Estimate mu
+        subject to constraints:
+            (1a) O_{B(i,j)} - (mu P mu.T)_{B(i,j)} = 0, for i != j, where B(i,j)
+                is the block of entries corresponding to sources i,j
+            (1b) np.sum( mu P, 1 ) = diag(O)
+        
+        (2) Source dependencies:
+            - First, estimate Z subject to the inverse form
+            constraint:
+                (2a) O_\Omega + (ZZ.T)_\Omega = 0, \Omega is the deps mask
+            - Then, compute Q = mu P mu.T
+            - Finally, estimate mu subject to mu P mu.T = Q and (1b)
+        """
+        self.config = recursive_merge_dicts(self.config, kwargs, 
+            misses='ignore')
+
+        if self.inv_form:
+            # Compute O, O^{-1}, and initialize params
+            if self.config['verbose']:
+                print("Computing O^{-1}...")
+            self._generate_O_inv(L)
+            self._init_params()
+
+            # Estimate Z, compute Q = \mu P \mu^T
+            if self.config['verbose']:
+                print("Estimating Z...")
+            self._train(self.loss_inv_Z)
+            self.Q = torch.from_numpy(self.get_Q()).float()
+
+            # Estimate \mu
+            if self.config['verbose']:
+                print("Estimating \mu...")
+            self._train(self.loss_inv_mu)
         else:
-            alphas_col = alphas[:,0]
-        return torch.clamp(alphas_col, min=0.05, max=0.95)
-    
-    def accs(self):
-        """The float numpy array of LF accuracies P(\lf_i=p_i|Y=p_i)."""
-        accs = self._alphas() * self._betas()
-        return accs.numpy()    
-    
-    def predict_proba(self, L):
-        """Get conditional probabilities P(Y | L) given the learned model
+            # Compute O and initialize params
+            if self.config['verbose']:
+                print("Computing O...")
+            self._generate_O(L)
+            self._init_params()
 
-        Args:
-            L: An [n, m] scipy.sparse matrix of labels 
-        Returns:
-            Y_p: An n x k numpy matrix of probabilistic labels (conditional
-                probabilities), i.e. $Y_p[i,j] = P(Y_i=j|L)$.
-        """
-        L = self._check_L(L)
-        # Note we cast to dense here
-        L = torch.from_numpy(L.todense()).float()
-        n = L.shape[0]
+            # Estimate \mu
+            if self.config['verbose']:
+                print("Estimating \mu...")
+            self._train(self.loss_mu)
 
-        # Here we iterate over the values of Y in {1,...,k}, forming
-        # an [n, k] matrix of unnormalized predictions
-        # Note in the unipolar setting:
-        #   P(\lambda_j=k|y_t=k, \lambda_j != 0) = \alpha_i
-        #   P(\lambda_j=k|y_t=l != k, \lambda_j != 0) = 1 - \alpha_i
-        # So the computation is the same as in the binary case, except we
-        # compute
-        #   \theta^T \ind \{ \lambda_j != 0 \} \ind^{\pm} \{ \lambda_j = k \}
-        log_odds_alphas = torch.log(self._alphas() / (1-self._alphas())).float()
-        Y_p = torch.zeros((n, self.k))
-        for y_k in range(1, self.k):
-            L_y = torch.where(
-                (L != y_k) & (L != 0), torch.full((n, self.m), -1) , L)
-            L_y = torch.where(L_y == y_k, torch.full((n, self.m), 1), L_y)
-            Y_p[:, y_k-1] = L_y @ log_odds_alphas
-        
-        # Add the class balance factor if not balanced
-        Y_cb = self.class_balance.detach().repeat(n,1).float()
-        Y_p += torch.log( Y_cb / (1-Y_cb) )
-
-        # Take the softmax to return an [n, k] numpy array
-        return F.softmax(Y_p, dim=1).numpy()
-
-    def get_accs_score(self, accs):
-        """Returns the *averaged squared estimation error."""
-        return np.linalg.norm(self.accs() - accs)**2 / self.m
-
-    def train(self, L_train, Y_train=None, L_dev=None, Y_dev=None, accs=None, 
-        **kwargs):
-        """Learns the accuracies of the labeling functions from L_train
-
-        Args:
-            L_train:
-            Y_train:
-            L_dev:
-            Y_dev:
-            accs: An M-length list of the true accuracies of the LFs if known
-
-        Note that Y_train, L_dev, and Y_dev are not required for any 
-        calculations. They are simply used for printing additional diagnostic 
-        information if they are provided.
-        """
-        self.config = recursive_merge_dicts(self.config, kwargs, misses='ignore')
+    def _train(self, loss_fn):
+        """Train model (self.parameters()) by optimizing the provided loss fn"""
         train_config = self.config['train_config']
-        optimizer_config = train_config['optimizer_config']
-
-        Y_dev = self._to_torch(Y_dev)
-
-        # Initialize class parameters based on L_train
-        L_train = self._check_L(L_train, init=True)
-
-        # Compute overlaps matrix self.O
-        self._compute_overlaps_matrix(L_train)
-
-        # Initialize params
-        self._init_params(
-            train_config['mu_init'],
-            train_config['learn_class_balance'],
-            train_config['class_balance_init'])
 
         # Set optimizer as SGD w/ momentum
+        optimizer_config = self.config['train_config']['optimizer_config']
         optimizer = optim.SGD(
-            self.parameters(), 
+            self.parameters(),
             **optimizer_config['optimizer_common'],
             **optimizer_config['sgd_config']
         )
-        
+
         # Train model
         for epoch in range(train_config['n_epochs']):
             optimizer.zero_grad()
             
             # Compute gradient and take a step
             # Note that since this uses all N training points this is an epoch!
-            loss = self.loss(l2=train_config['l2'])
+            loss = loss_fn()
             if torch.isnan(loss):
                 raise Exception("Loss is NaN. Consider reducing learning rate.")
 
@@ -285,24 +184,4 @@ class LabelModel(Classifier):
                 (epoch % train_config['print_every'] == 0 
                 or epoch == train_config['n_epochs'] - 1)):
                 msg = f"[Epoch {epoch}] Loss: {loss.item():0.6f}"
-                if accs is not None:
-                    accs_score = self.get_accs_score(accs)
-                    msg += f"\tAccs mean sq. error = {accs_score}"
                 print(msg)
-
-        if self.config['verbose']:
-            print('Finished Training')
-            
-            if self.config['show_plots']:
-                print(lf_summary(L=L_train, Y=Y_train, est_accs=self._alphas()))
-    
-                if self.k == 2:
-                    Y_p_train = self.predict_proba(L_train)
-                    plot_probabilities_histogram(Y_p_train[:, 0], 
-                        title="Training Set Predictions")
-
-            if L_dev is not None and Y_dev is not None:
-                Y_ph_dev = self.predict(L_dev)
-
-                print("Confusion Matrix (Dev)")
-                mat = confusion_matrix(Y_ph_dev, Y_dev, pretty_print=True)
