0848521306bf95674823c28fec4e1664bcf350ce
Braden Hancock <hancock.braden@gmail.com>
2018-08-01 00:48:56 -0700

    Fix TaskGraph import order

--- metal/multitask/__init__.py
+++ metal/multitask/__init__.py
@@ -1,4 +1,4 @@
-from .utils import MTMetalDataset
 from .task_graph import TaskHierarchy, SingleTaskGraph
+from .utils import MTMetalDataset
 from .mt_classifier import MTClassifier
 from .mt_end_model import MTEndModel
--- metal/multitask/task_graph.py
+++ metal/multitask/task_graph.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
 
-from collections import defaultdict
-
 import numpy as np
 import networkx as nx
 
