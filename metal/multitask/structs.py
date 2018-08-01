8145c3c937b10ab25aa4ecb7b2ef37823b810531
Braden Hancock <hancock.braden@gmail.com>
2018-07-31 20:31:15 -0700

    Restore task_graph and its test

---
+++ metal/multitask/structs.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+
+class TaskGraph(object):
+    """A directed acyclic graph of tasks with utility methods.
+    TODO: Update this docstring
+
+    Each task t=0,...,T-1 has categorical task labels Y_t=0,...
+    For example, we might have three binary prediction tasks:
+
+                                    t_0
+                                   /   \
+                                 (1)   (2)
+                                 t_1   t_2
+                                 / \   / \
+                                1   2 1   2
+
+    We reserve the value 0 at each node to mean abstain (for labeling) or the
+    null label (for gold labels).
+
+    The input to initialize this class is:
+        - edges: A set of tuples (p, c) representing a directed edge from task
+            p -> c.
+        - cardinalities: A list of cardinalities of tasks 0,1,...,T-1 (not 
+            counting the "abstain" value in non-root nodes).
+    """
+    def __init__(self, edges, cardinalities):
+        self.edges = edges
+        self.K_t = cardinalities     # Cardinalities by task
+        self.K = max(cardinalities)  # Max cardinality
+        self.T = len(cardinalities)  # Total number of tasks
+
+        self.children = defaultdict(list)
+        self.parents = defaultdict(list)
+        self.neighbors = defaultdict(list)
+        for p, c in self.edges:
+            self.children[p].append(c)
+            self.parents[c].append(p)
+            self.neighbors[c].append(p)
+            self.neighbors[p].append(c)
+
+        # Sort parents, children, and neighbors by index
+        # Note that this means that task label k corresponds to child k-1
+        for p, children in self.children.items():
+            self.children[p] = sorted(list(set(children)))
+        for c, parents in self.parents.items():
+            self.parents[c] = sorted(list(set(parents)))
+        for x, neighbors in self.neighbors.items():
+            self.neighbors[x] = sorted(list(set(neighbors)))
+
+        # Get depth (max-length path) of DAG
+        # Note that a single node has depth 0)
+        # TODO: implement _compute_depth()
+        self.depth = None
+        # self.depth = self._compute_depth()
+
+
+    def __eq__(self, other):
+        return self.edges == other.edges and self.K_t == other.K_t
+
+    def _compute_depth(self):
+        raise NotImplementedError
+
+class SingleTaskGraph(TaskGraph):
+    """
+    A TaskTree with a single task.
+    
+    For example, a flat binary classification task can be represented as a
+    SingleTaskTree with one node whose cardinality is 2.
+    """    
+    def __init__(self, k=2):
+        cardinalities = [k]
+        edges = []
+        super().__init__(edges, cardinalities)
---
+++ tests/metal/multitask/test_task_graph.py
@@ -0,0 +1,51 @@
+import sys
+import unittest
+
+from metal.multitask.structs import TaskGraph, SingleTaskGraph
+
+class TaskGraphTest(unittest.TestCase):
+
+    def test_single_task(self):
+        tg = SingleTaskGraph(k=3)
+        self.assertTrue(tg == TaskGraph([], [3]))
+        # self.assertTrue(tg.depth == 0)
+
+    def test_binary_tree(self):
+        cardinalities = [2,2,2]
+        edges = [(0,1), (0,2)]
+        tg = TaskGraph(edges, cardinalities)
+        self.assertTrue(tg.parents[0] == [])
+        self.assertTrue(tg.parents[1] == [0])
+        self.assertTrue(tg.parents[2] == [0])
+        self.assertTrue(tg.children[0] == [1,2])
+        self.assertTrue(tg.children[1] == [])
+        self.assertTrue(tg.children[2] == [])
+        # self.assertTrue(tg.depth == 1)
+        
+    def test_nonbinary_tree(self):
+        cardinalities = [3,2,2,2]
+        edges = [(0,1), (0,2), (0,3)]
+        tg = TaskGraph(edges, cardinalities)
+        self.assertTrue(tg.parents[1] == [0])
+        self.assertTrue(tg.children[0] == [1,2,3])
+        # self.assertTrue(tg.depth == 1)
+
+    def test_binary_tree_depth3(self):
+        cardinalities = [2,2,2,2,2,2,2]
+        edges = [(0,1), (0,2), (1,3), (1,4), (2,5), (2,6)]
+        tg = TaskGraph(edges, cardinalities)
+        self.assertTrue(tg.parents[1] == [0])
+        self.assertTrue(tg.children[1] == [3, 4])
+        # self.assertTrue(tg.depth == 2)
+
+    def test_unbalanced_tree(self):
+        cardinalities = [2,2,2]
+        edges = [(0,1), (1,2)]
+        tg = TaskGraph(edges, cardinalities)
+        self.assertTrue(tg.parents[1] == [0])
+        self.assertTrue(tg.children[1] == [2])
+        # self.assertTrue(tg.depth == 2)
+
+        
+if __name__ == '__main__':
+    unittest.main()
