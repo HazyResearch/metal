51b385cd8845191c00eccf1d72029a91e31b94bd
Braden Hancock <hancock.braden@gmail.com>
2018-08-06 22:52:26 -0700

    Fix end model test by increasing synthetic data dev/test size

--- tests/metal/end_model/test_end_model.py
+++ tests/metal/end_model/test_end_model.py
@@ -12,7 +12,7 @@ class EndModelTest(unittest.TestCase):
         # Set seed
         np.random.seed(1)
 
-        N = 1400
+        N = 2000
 
         X = np.random.random((N,2)) * 2 - 1
         Y = (X[:,0] > X[:,1] + 0.25).astype(int) + 1
@@ -20,30 +20,30 @@ class EndModelTest(unittest.TestCase):
         X = torch.tensor(X, dtype=torch.float)
         Y = torch.tensor(Y, dtype=torch.long)
 
-        Xs = [X[:1000], X[1000:1200], X[1200:]]
-        Ys = [Y[:1000], Y[1000:1200], Y[1200:]]
+        Xs = [X[:1000], X[1000:1500], X[1500:]]
+        Ys = [Y[:1000], Y[1000:1500], Y[1500:]]
         cls.single_problem = (Xs, Ys)
 
     def test_logreg(self):
-        em = LogisticRegression(seed=1, verbose=False, input_dim=2)
+        em = LogisticRegression(seed=1, input_dim=2, verbose=False)
         Xs, Ys = self.single_problem
         em.train(Xs[0], Ys[0], Xs[1], Ys[1], n_epochs=5)
         score = em.score(Xs[2], Ys[2], verbose=False)
         self.assertGreater(score, 0.95)
 
     def test_singletask(self):
-        em = EndModel(seed=1, verbose=False, batchnorm=False, dropout=0.0,
-            layer_output_dims=[2,8,4])
+        em = EndModel(seed=1, batchnorm=False, dropout=0.0, verbose=False, 
+            layer_output_dims=[2,10])
         Xs, Ys = self.single_problem
         em.train(Xs[0], Ys[0], Xs[1], Ys[1], n_epochs=5)
         score = em.score(Xs[2], Ys[2], verbose=False)
         self.assertGreater(score, 0.95)
 
     def test_singletask_extras(self):
-        em = EndModel(seed=1, batchnorm=True, dropout=0.01, verbose=False,
-            layer_output_dims=[2,8,4])
+        em = EndModel(seed=1, batchnorm=True, dropout=0.01, verbose=True,
+            layer_output_dims=[2,10])
         Xs, Ys = self.single_problem
-        em.train(Xs[0], Ys[0], Xs[1], Ys[1], n_epochs=5, lr=0.005)
+        em.train(Xs[0], Ys[0], Xs[1], Ys[1], n_epochs=5)
         score = em.score(Xs[2], Ys[2], verbose=False)
         self.assertGreater(score, 0.95)
         
