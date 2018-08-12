83b3b90d533abbca7a404c2b3afb5a6952aa6922
maxlam <maxlam@dawn6.stanford.edu>
2018-08-11 21:53:11 -0700

    Revert back to synthetic

---
+++ synthetic/__init__.py
@@ -0,0 +1,6 @@
+from .generate import (
+    SingleTaskTreeDepsGenerator,
+    HierarchicalMultiTaskTreeDepsGenerator,
+    gaussian_bags_of_words
+)   
+from .words1k import vocab1k
--- synthetics/__init__.py
+++
@@ -1,2 +0,0 @@
-from .generate import generate_single_task_unipolar, gaussian_bags_of_words
-from .words1k import vocab1k
