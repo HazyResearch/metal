3ec8d97ca0da3dd183a86d94d7b86c60b73e9910
fredsala <fredsala@stanford.edu>
2018-08-19 15:07:08 -0700

    Add a very naive SPA to compute exact probabilities for full model, use these to generate conds

--- synthetic/generate.py
+++ synthetic/generate.py
@@ -6,6 +6,7 @@ import numpy as np
 from numpy.random import random, choice
 from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
 import torch
+import networkx as nx
 
 from metal.metrics import accuracy_score, coverage_score
 from metal.multitask.task_graph import TaskHierarchy
@@ -50,8 +51,8 @@ class SingleTaskTreeDepsGenerator(object):
     Note that k = the # of true classes; thus source labels are in {0,1,...,k}
     because they include abstains.
     """
-    def __init__(self, n, m, k=2, class_balance='random', theta_range=(0, 1.5), 
-        edge_prob=0.0, theta_edge_range=(-1,1), **kwargs):
+    def __init__(self, n, m, k=2, class_balance='random', theta_range=(0.1, 1), 
+        edge_prob=0.0, theta_edge_range=(0.1,1), **kwargs):
         self.n = n
         self.m = m
         self.k = k
@@ -86,30 +87,168 @@ class SingleTaskTreeDepsGenerator(object):
     
         Also create helper data struct mapping child -> parent.
         """
+        self.G = nx.Graph()
         self.E, self.parent = [], {}
         for i in range(self.m):
             if random() < edge_prob and i > 0:
                 p_i = choice(i)
                 self.E.append((p_i, i))
                 self.parent[i] = p_i
+                self.G.add_edge(i,p_i)
 
     def _generate_params(self, theta_range, theta_edge_range):
         self.theta = defaultdict(float)
         for i in range(self.m):
-            t_min, t_max = min(theta_range), max(theta_range)
-            self.theta[i] = (t_max - t_min) * random(self.k+1) + t_min
+            for y in range(1, self.k+1):
+                t_min, t_max = min(theta_range), max(theta_range)
+                self.theta[(i,y)] = (t_max - t_min) * random(self.k) + t_min
 
         # Choose random weights for the edges
+        # Note: modifications to get the correct exponential model family
+        #       formulation from the arxiv paper
         te_min, te_max = min(theta_edge_range), max(theta_edge_range)
         for (i,j) in self.E:
-            w_ij = (te_max - te_min) * random() + te_min
-            self.theta[(i,j)] = w_ij
-            self.theta[(j,i)] = w_ij
+            for y1 in range(1, self.k+1):
+                for y2 in range(1, self.k+1):
+                    #w_ij = (te_max - te_min) * random() + te_min
+                    w_ij = (te_max - te_min) * random(self.k) + te_min
+                    self.theta[((i, j), y1, y2)] = w_ij
+                    #self.theta[((j, i), y1, y2)] = w_ij
+
+        for key in self.theta:
+            print(key, " ", self.theta[key])
     
-    def _P(self, i, li, j, lj, y):
-        return np.exp( 
-            self.theta[i][y] * indpm(li, y) + self.theta[(i,j)] * indpm(li, lj))
-    
+
+    def get_Z(self, y):
+        # verify all partition functions the same:
+        '''for i in range(self.m):
+            print(np.sum(self.naive_SPA(i,y)))'''
+
+        return np.sum(self.naive_SPA(0,y))
+
+    def naive_SPA(self, i, y, j=None, jval=None, verbose=False):
+        # this contains our nodes:
+        G_i_set = nx.node_connected_component(self.G, i)
+        G_i     = self.G.subgraph(G_i_set)
+        G_i_bfs = nx.bfs_tree(G_i, i)
+
+        if verbose: print("\n\nDoing a naive SPA for node ", i)
+        if verbose: print("our graph ", G_i_bfs.edges())
+
+        # build nodes at each depth of our tree
+        # defines our elimination ordering
+        dist_list = nx.shortest_path_length(G_i_bfs, source=i)
+        depths = dict()
+        max_depth = 0
+        messages = dict()
+
+        for node in dist_list:
+            messages[node] = np.zeros(self.k+1)
+
+            if dist_list[node] not in depths:
+                depths[dist_list[node]] = [node]
+                if dist_list[node] > max_depth:
+                    max_depth = dist_list[node]
+            else:
+                depths[dist_list[node]].append(node)
+
+        parents = nx.predecessor(G_i_bfs, i)
+
+        if verbose: print("depths = ", depths)
+        if verbose: print("parents = ", parents)
+
+        # now we do sum product
+        while max_depth > 0:
+            for node in depths[max_depth]:
+                # compute the message node->parent:
+                # this is a function m(parent=val)
+
+                if verbose: print("working on node ", node, " with parent ", parents[node][0])
+
+                for val_p in range(self.k+1):
+                    if verbose: print("For val_p = ", val_p)
+                    mess = 0
+
+                    if node == j:
+                        val_range = [jval]
+                    else:
+                        val_range = range(0, self.k+1)
+
+                    for val in val_range:
+                        mess_local = 1 # local unary term
+                        if val > 0:
+                            mess_local = np.exp(self.theta[(node, val)][y-1]) 
+
+                        mess_edge = 1 # local edge term (node, parent)    
+                        if val > 0 and val_p > 0:
+                            # we need to figure out the exact order of these things, in the original graph:
+                            if (parents[node][0], node) in self.E:
+                                mess_edge  = np.exp(self.theta[((parents[node][0], node), val_p, val)][y-1])
+                            else:
+                                mess_edge  = np.exp(self.theta[((node, parents[node][0]), val, val_p)][y-1])
+
+                        mess_prod  = 1 # product of all incoming messages at node
+                        for edge in nx.edges(G_i_bfs, node):
+                            if edge[1] != parents[node][0]:
+                                if verbose: print("incoming message along edge ", edge[1], " val = ", val, " mess =", messages[edge[1]][val])
+                                mess_prod *= messages[edge[1]][val]
+
+                        mess += mess_local * mess_edge * mess_prod # sum
+                        if verbose: print("we added ", mess_local * mess_edge * mess_prod, "\n")
+                                   
+                    messages[node][val_p] = mess
+
+                if verbose: print("finished message at node ", node)
+                if verbose: print("message was ", messages[node], "\n")
+            max_depth -= 1
+
+        # now we're left with the messages just to i:
+        if verbose: print("now at top, getting the final marginal for i = ", i)
+
+        message_i = np.zeros(self.k+1)
+        for val in range(0, self.k+1):
+            mess_local = 1
+            if val > 0:
+                mess_local = np.exp(self.theta[(i, val)][y-1])
+            mess_prod = 1
+            for edge in nx.edges(G_i_bfs, i):
+                mess_prod *= messages[edge[1]][val]
+                if verbose: print("incoming message along edge ", edge[1], " val = ", val, " mess =", messages[edge[1]][val])
+            message_i[val] = mess_local * mess_prod
+            
+        if verbose: print("Final marginal: ", message_i)
+        if verbose: print("\n\n")
+        return message_i
+
+    # these are the real probabilities for each node:
+    def P_vals_true(self):
+        self.p_solo = defaultdict(float)
+
+        for y in range(1, self.k+1):
+            Z = self.get_Z(y)
+
+            for i in range(self.m):
+                print("Labeler = ", i)
+                for val in range(self.k+1):
+                    self.p_solo[(i,val,y)] = self.naive_SPA(i,y)[val] / Z
+                    print("P(L=", val, ", Y=",y,") = ", self.p_solo[(i,val,y)])
+                    
+    # these are the real joint probabilities for each pair of nodes:
+    def P_joints_true(self):
+        self.p_joints = defaultdict(float)
+        
+        for y in range(1,self.k+1):
+            Z = self.get_Z(y)
+
+            for i in range(self.m):
+                for j in range(i+1, self.m):
+                    print("Labelers = ", (i,j))
+
+                    for val1 in range(self.k+1):
+                        for val2 in range(self.k+1):
+                            self.p_joints[(i,val1,j,val2,y)] = self.naive_SPA(i,y,j=j,jval=val2)[val1] / Z
+                            print("P(L_", i, "=", val1, ", L_", j, "=", val2, " | Y = ", y, ") = ", self.p_joints[(i,val1,j,val2,y)])
+
     def P_conditional(self, i, li, j, lj, y):
         """Compute the conditional probability 
             P_\theta(li | lj, y) 
@@ -123,13 +262,16 @@ class SingleTaskTreeDepsGenerator(object):
             - a class-conditional LF accuracy parameter \theta_{i|y}
             - a symmetric LF correlation paramter \theta_{i,j}
         """
-        Z = np.sum([self._P(i, _li, j, lj, y) for _li in range(self.k+1)])
-        return self._P(i, li, j, lj, y) / Z
+        return self.p_joints[(i,li,j,lj,y)] / self.p_solo[(j,lj,y)]
     
     def _generate_label_matrix(self):
         """Generate an n x m label matrix with entries in {0,...,k}"""
         self.L = np.zeros((self.n, self.m))
         self.Y = np.zeros(self.n, dtype=np.int64)
+
+        self.P_vals_true()
+        self.P_joints_true()
+
         for i in range(self.n):
             y = choice(self.k, p=self.p) + 1  # Note that y \in {1,...,k}
             self.Y[i] = y
