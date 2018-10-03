b4b3d519084b9791bd8b77630d7b4ef8241e3d3c
ajratner <ajratner@gmail.com>
2018-10-03 10:05:50 -0700

    - Expanding JunctionTree.iter_observed to include all cliques - Adding a function to JunctionTree for maximal cliques mask

--- metal/label_model/graph_utils.py
+++ metal/label_model/graph_utils.py
@@ -1,6 +1,7 @@
-from itertools import product
+from itertools import combinations, product
 
 import networkx as nx
+import numpy as np
 
 
 class DependenciesGraph(object):
@@ -165,7 +166,7 @@ class JunctionTree(object):
         for vals in product(*val_ranges_s):
             yield dict(zip(var_ids, vals))
 
-    def iter_observed(self):
+    def iter_observed(self, offset=1):
         """Iterates over (i, vals) where i is an index and vals is a dictionary
         mapping from LF indices to values, iterating over all observed values:
             - The m LFs
@@ -180,17 +181,22 @@ class JunctionTree(object):
         if self.higher_order_cliques:
             for ci in self.G.nodes():
                 cids = self.get_members(ci) - {self.m}
-                if cids not in cliques:
-                    cliques.append(set(cids))
+
+                # Iterate through the full powerset of observed cliques
+                for r in range(2, len(cids) + 1):
+                    for cids_ss in combinations(cids, r):
+                        cids_ss = set(cids_ss)
+                        if cids_ss not in cliques:
+                            cliques.append(set(cids_ss))
 
         # Iterate over the index, minimal set of values as dict
         idx = -1
         for cids in cliques:
-            for vals in self.iter_vals(cids, offset=1):
+            for vals in self.iter_vals(cids, offset=offset):
                 idx += 1
                 yield idx, vals
 
-    def iter_hidden(self):
+    def iter_hidden(self, offset=1):
         """Iterates over (i, vals) where i is an index and vals is a dictionary
         mapping from LF indices to values, iterating over all hidden values:
             - Y
@@ -210,9 +216,22 @@ class JunctionTree(object):
         # Iterate over the index, minimal set of values as dict
         idx = -1
         for cids in cliques:
-            for vals in self.iter_vals(cids, offset=1):
+            for vals in self.iter_vals(cids, offset=offset):
                 idx += 1
                 yield idx, vals
+
+    def observed_maximal_mask(self):
+        """Returns a binary mask selecting just those observed indices
+        corresponding to maximal cliques (nodes) in the junction tree."""
+        mask = np.zeros(self.O_d)
+        for idx, vals in self.iter_observed():
+            ci_max = list(self.get_maximal_cliques(vals.keys()))
+            assert len(ci_max) == 1
+            ci_max = ci_max[0]
+            ci_max_members = self.get_members(ci_max)
+            if len(vals) + 1 == len(ci_max_members):
+                mask[idx] = 1
+        return mask
 
     @property
     def ind_model(self):
