b50bf0c3fd1173df09c37168c00dbc7babfb48f5
jdunnmon <jdunnmon@gmail.com>
2018-11-19 14:18:07 -0800

    fixed formatting

--- metal/end_model/end_model.py
+++ metal/end_model/end_model.py
@@ -83,7 +83,7 @@ class EndModel(Classifier):
         # Construct loss module
         weight = self.config["train_config"]["loss_weights"]
         reduction = self.config["train_config"]["loss_fn_reduction"]
-        self.criteria = SoftCrossEntropyLoss(weight=weight,reduction=reduction)
+        self.criteria = SoftCrossEntropyLoss(weight=weight, reduction=reduction)
 
     def _build_input_layer(self, input_module):
         if input_module is None:
