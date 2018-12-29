1bad49ba9a0737ac2315bfd129d9d1e001f54ef6
ajratner <ajratner@gmail.com>
2018-12-28 16:33:16 -0800

    ClassBalanceModel working from L

--- tests/metal/label_model/test_class_balance.py
+++ tests/metal/label_model/test_class_balance.py
@@ -10,10 +10,6 @@ from metal.label_model.class_balance import ClassBalanceModel
 sys.path.append("../synthetic")
 
 
-# TODOs:
-# (3) Noisy tests- starting from L
-
-
 class ClassBalanceModelTest(unittest.TestCase):
     def _set_seed(self, seed):
         torch.manual_seed(seed)
@@ -54,18 +50,37 @@ class ClassBalanceModelTest(unittest.TestCase):
             cpts.append(a @ np.diag(1 / a.sum(axis=0)))
         return np.array(cpts)
 
+    def _generate_L(self, p_Y, C, n, abstains=False):
+        """Generate a label matrix L, with entries in {0,1,...,k} if
+        abstains=True, else in {1,...,k}, given the true class balance, p_Y, and
+        a conditional probabilities table C of m cond. ind. LFs"""
+        k = len(p_Y)
+        m = C.shape[0]
+
+        # Generate true data labels for n data points
+        Y = np.random.choice(range(1, k + 1), n, p=p_Y)
+
+        # Generate label matrix L with entries in {0,1,...,k} if abstains=True,
+        # else in {1,...,k}
+        lf_0 = 0 if abstains else 1
+        L = np.zeros((n, m))
+        for i, y in enumerate(Y):
+            for j in range(m):
+                L[i, j] = np.random.choice(range(lf_0, k + 1), p=C[j, :, y - 1])
+        return L
+
+    def _test_model(self, model, p_Y, O=None, L=None, tol=1e-3, verbose=True):
+        model.train_model(O=O, L=L)
+        if verbose:
+            print(f"True class balance: {p_Y}")
+            print(f"Estimated class balance: {model.class_balance}")
+        self.assertLess(np.mean(np.abs(p_Y - model.class_balance)), tol)
+
     def _test_class_balance_estimation(
         self, k, m, abstains=False, verbose=True
     ):
         model = ClassBalanceModel(k, abstains=abstains)
-
-        # Generate the true class balance
         p_Y = self._generate_class_balance(k)
-
-        # Generate the true conditional probability tables for the m
-        # conditionally independent LFs
-        # Note: We bias these along the diagonal to enforce assumption that the
-        # LFs are better than random!
         C = self._generate_cond_probs(k, m, bias_diag=True, abstains=abstains)
 
         # Compute O; mask out diagonal entries
@@ -75,11 +90,20 @@ class ClassBalanceModelTest(unittest.TestCase):
         O[1 - mask] = 0
 
         # Test recovery of the class balance
-        model.train_model(O=O)
-        if verbose:
-            print(f"True class balance: {p_Y}")
-            print(f"Estimated class balance: {model.class_balance}")
-        self.assertLess(np.mean(np.abs(p_Y - model.class_balance)), 1e-3)
+        self._test_model(model, p_Y, O=O)
+
+    def _test_class_balance_estimation_noisy(
+        self, k, m, n, abstains=False, verbose=True
+    ):
+        model = ClassBalanceModel(k, abstains=abstains)
+        p_Y = self._generate_class_balance(k)
+        C = self._generate_cond_probs(k, m, bias_diag=True, abstains=abstains)
+
+        # Generate label matrix L
+        L = self._generate_L(p_Y, C, n, abstains=abstains)
+
+        # Test recovery of the class balance
+        self._test_model(model, p_Y, L=L, tol=1e-2)
 
     def test_class_balance_estimation_2(self):
         self._set_seed(123)
@@ -97,6 +121,10 @@ class ClassBalanceModelTest(unittest.TestCase):
     def test_class_balance_estimation_2_abstains(self):
         self._set_seed(123)
         self._test_class_balance_estimation(2, 25, abstains=True)
+
+    def test_class_balance_estimation_2_noisy(self):
+        self._set_seed(123)
+        self._test_class_balance_estimation_noisy(2, 25, 10000, abstains=True)
 
 
 if __name__ == "__main__":
