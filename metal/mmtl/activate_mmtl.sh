badf28a2d30b23a857b88b363275fa91153a4c86
Vincent Chen <vincentschen@yahoo.com>
2019-02-06 12:31:12 -0800

    fix relative path to METALHOME

--- metal/mmtl/activate_mmtl.sh
+++ metal/mmtl/activate_mmtl.sh
@@ -1,4 +1,4 @@
-export METALHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export METALHOME="$( cd "$( dirname "../../../${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="$PYTHONPATH:$METALHOME"
 echo "Added Snorkel MeTaL repository ($METALHOME) to \$PYTHONPATH."
 
