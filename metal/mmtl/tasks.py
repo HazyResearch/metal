6cace5f2832de4ccf9812bc5039d646f0089a3e8
Braden Hancock <hancock.braden@gmail.com>
2019-02-06 14:14:03 -0800

    Make Task accept a list of DataLoaders corresponding to [train, dev, test]

--- metal/mmtl/tasks.py
+++ metal/mmtl/tasks.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional
+from typing import Callable, List
 
 from torch.utils.data import DataLoader
 
@@ -7,21 +7,22 @@ class Task(object):
     """A task for use in an MMTL MetalModel
 
     Args:
-        name: (str) The name of the task
-        head_name: (str) The name of the task head to use
+        name: The name of the task
+        head_name: The name of the task head to use
             TODO: replace this with a more fully-featured path through the network
-        data: (DataLoader) The data (instances and labels) to feed through the network
-        scorers: (list) A list of Scorers that return metrics_dict objects.
+        data: A list of DataLoaders (instances and labels) to feed through the network.
+            The list contains [train, dev, test].
+        scorers: A list of Scorers that return metrics_dict objects.
     """
 
     def __init__(
         self,
         name: str,
         head_name: str,
-        data: DataLoader,
+        data_loaders: List[DataLoader],
         scorers: List[Callable] = None,
     ) -> None:
         self.name = name
-        self.data = data
+        self.data_loaders = data_loaders
         self.head_name = head_name
         self.scorers = scorers
