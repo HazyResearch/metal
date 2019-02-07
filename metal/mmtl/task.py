6f61fbf2402cef5097e0cf6392a5ab39461ced60
Braden Hancock <hancock.braden@gmail.com>
2019-02-07 10:35:43 -0800

    Update Task definition to include modules instead of module names

--- metal/mmtl/task.py
+++ metal/mmtl/task.py
@@ -1,5 +1,6 @@
 from typing import Callable, List
 
+import torch.nn as nn
 from torch.utils.data import DataLoader
 
 
@@ -8,9 +9,9 @@ class Task(object):
 
     Args:
         name: The name of the task
-        input_name: The name of the input module to use
-        head_name: The name of the task head module to use
-            TODO: replace this with a more fully-featured path through the network
+        TODO: replace this with a more fully-featured path through the network
+        input_module: The input module
+        head_module: The task head module
         data: A list of DataLoaders (instances and labels) to feed through the network.
             The list contains [train, dev, test].
         scorers: A list of Scorers that return metrics_dict objects.
@@ -19,17 +20,16 @@ class Task(object):
     def __init__(
         self,
         name: str,
-        input_name: str,
-        head_name: str,
+        input_module: nn.Module,
+        head_module: nn.Module,
         data_loaders: List[DataLoader],
         scorers: List[Callable] = None,
     ) -> None:
         if len(data_loaders) != 3:
             msg = "Arg data_loaders must be a list of length 3 [train, valid, test]"
             raise Exception(msg)
-
         self.name = name
-        self.input_name = input_name
-        self.head_name = head_name
+        self.input_module = input_module
+        self.head_module = head_module
         self.data_loaders = data_loaders
         self.scorers = scorers
