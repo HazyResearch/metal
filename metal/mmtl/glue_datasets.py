e58372b225303c2bc59114afb909bd4c62fe5899
Ines Chami <ineschami@hotmail.com>
2019-02-07 01:28:59 -0800

    reformatting for travis tests

--- metal/mmtl/glue_datasets.py
+++ metal/mmtl/glue_datasets.py
@@ -7,7 +7,8 @@ from tqdm import tqdm
 
 class GlueDataset(data.Dataset):
     """
-    Torch dataset object for Glue task. Each specific task should override the preprocess_data and load_data methods.
+    Torch dataset object for Glue task. Each specific task should override
+    the preprocess_data and load_data methods.
     """
 
     def __init__(self, src_path, tokenizer):
@@ -38,12 +39,15 @@ class GlueDataset(data.Dataset):
         raise NotImplementedError
 
     def get_dataloader(self, max_len=-1, batch_size=1):
-        return data.DataLoader(self, collate_fn=lambda batch: self.collate_fn(batch, max_len), batch_size=batch_size, shuffle=True)
+        return data.DataLoader(self, collate_fn=lambda batch: self.collate_fn(
+                batch, max_len),
+                               batch_size=batch_size,
+                               shuffle=True)
 
     def collate_fn(self, batch, max_len):
         batch_size = len(batch)
         max_sent_len = int(np.max([len(tok) for ((tok, seg), _) in batch]))
-        if max_len > 0 and max_len < max_sent_len:
+        if (max_len > 0) and (max_len < max_sent_len):
             max_sent_len = max_len
         idx_matrix = np.zeros((batch_size, max_sent_len), dtype=np.int)
         seg_matrix = np.zeros((batch_size, max_sent_len), dtype=np.int)
@@ -51,7 +55,6 @@ class GlueDataset(data.Dataset):
 
         for idx1 in np.arange(len(batch)):
             (tokens, segments), labels = batch[idx1]
-            # label_matrix[idx1, :] = labels
             label_matrix[idx1, :] = labels
             for idx2 in np.arange(len(tokens)):
                 if idx2 >= max_sent_len:
@@ -80,11 +83,13 @@ class QNLIDataset(GlueDataset):
             self.raw_data['label'] = ['entailment'] * self.__len__()
 
     def preprocess_data(self):
-        # for i, row in tqdm(list(self.raw_data.iterrows())):
         for i, row in tqdm(list(self.raw_data.iterrows())):
             tokenized_question = self.tokenizer.tokenize(row.question)
             tokenized_sentence = self.tokenizer.tokenize(row.sentence)
             tokenized_text = tokenized_question + tokenized_sentence
-            self.tokens.append(self.tokenizer.convert_tokens_to_ids(tokenized_text))
-            self.segments.append(([0] * len(tokenized_question)) + ([1] * len(tokenized_sentence)))
-            self.labels.append([(1 * (self.raw_data.label[i] == 'entailment')) + 1]) # + [1 * (self.raw_data.label[i] == 'not_entailment')])
+            self.tokens.append(
+                    self.tokenizer.convert_tokens_to_ids(tokenized_text))
+            self.segments.append(([0] * len(tokenized_question)) + ([1] * len(
+                    tokenized_sentence)))
+            self.labels.append(
+                    [(1 * (self.raw_data.label[i] == 'entailment')) + 1])
