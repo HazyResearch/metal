a04bd7f0dc092c45e51325630f2dca0a5b9fa584
Clara McCreery <chmccreery@gmail.com>
2019-02-08 12:53:18 -0800

    CoLA task

--- metal/mmtl/BERT_tasks.py
+++ metal/mmtl/BERT_tasks.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-from dataset import SST2Dataset
+from dataset import COLADataset, SST2Dataset
 from modules import BertBinaryHead, BertEncoder, BertMulticlassHead
 from pytorch_pretrained_bert import BertForMaskedLM, BertModel, BertTokenizer
+from sklearn.metrics import matthews_corrcoef
 from task import Task
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 
@@ -14,35 +15,35 @@ from metal.mmtl.scorer import Scorer
 from metal.mmtl.utils.dataset_utils import get_all_dataloaders
 
 
-"""
-dataloaders = createBertDataloader(
-    "SST-2",
-    batch_sz=8,
-    sent1_idx=0,
-    sent2_idx=-1,
-    label_idx=1,
-    header=1,
-    label_fn=lambda x: int(x) + 1,
-)
-"""
-
-
 def create_task(task_name):
     bert_model = "bert-base-uncased"
     bert_encoder = BertEncoder(bert_model)
 
     if task_name == "CoLA":
-        raise NotImplementedError
-    if task_name == "SST-2":
-        dataloaders = get_all_dataloaders(SST2Dataset, bert_model)
+        dataloaders = get_all_dataloaders(COLADataset, bert_model)
+
+        def matthews_corr(targets, predictions):
+            import pdb
+
+            pdb.set_trace()
+            predictions = np.argmax(predictions, 1)
+            matthews = matthews_corrcoef(targets, predictions)
+            return {"matthews_corr": matthews}
 
         return Task(
             task_name,
             dataloaders,
             bert_encoder,
             BertBinaryHead(),
-            [Scorer(standard_metrics=["accuracy"])],
+            Scorer(
+                standard_metrics=["accuracy"],
+                custom_train_funcs=[matthews_corr],
+                custom_valid_funcs=[matthews_corr],
+            ),
         )
+    if task_name == "SST-2":
+        dataloaders = get_all_dataloaders(SST2Dataset, bert_model)
+        return Task(task_name, dataloaders, bert_encoder, BertBinaryHead())
     elif task_name == "MNLI":
         raise NotImplementedError
     elif task_name == "RTE":
