fd30a56012803ec6d30df8f8b8d583040e12bcea
Vincent Chen <vincentschen@yahoo.com>
2019-02-08 12:41:15 -0800

    add STS-B in metal model task

--- metal/mmtl/BERT_tasks.py
+++ metal/mmtl/BERT_tasks.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-from dataset import SST2Dataset
-from modules import BertBinaryHead, BertEncoder, BertMulticlassHead
+import torch.nn.functional as F
+from dataset import SST2Dataset, STSBDataset
+from modules import BertBinaryHead, BertEncoder, BertMulticlassHead, BertRegressionHead
 from pytorch_pretrained_bert import BertForMaskedLM, BertModel, BertTokenizer
 from task import Task
 from torch.utils.data import DataLoader, Dataset, TensorDataset
@@ -12,6 +13,7 @@ import metal
 from metal.end_model import EndModel
 from metal.mmtl.scorer import Scorer
 from metal.mmtl.utils.dataset_utils import get_all_dataloaders
+from metal.mmtl.utils.metrics import pearson_corr, spearman_corr
 
 
 """
@@ -35,7 +37,6 @@ def create_task(task_name):
         raise NotImplementedError
     if task_name == "SST-2":
         dataloaders = get_all_dataloaders(SST2Dataset, bert_model)
-
         return Task(
             task_name,
             dataloaders,
@@ -43,6 +44,7 @@ def create_task(task_name):
             BertBinaryHead(),
             [Scorer(standard_metrics=["accuracy"])],
         )
+
     elif task_name == "MNLI":
         raise NotImplementedError
     elif task_name == "RTE":
@@ -54,7 +56,24 @@ def create_task(task_name):
     elif task_name == "MRPC":
         raise NotImplementedError
     elif task_name == "STS-B":
-        raise NotImplementedError
+        dataloaders = get_all_dataloaders(STSBDataset, bert_model)
+        scorer = Scorer(
+            standard_metrics=["train/loss", "valid/loss"],
+            custom_metric_fns=[pearson_corr, spearman_corr],
+        )
+
+        # x -> sigmoid -> [0,1], and compute mse_loss (y \in [0,1])
+        loss_hat_func = lambda x, y: F.mse_loss(torch.sigmoid(x), y)
+        return Task(
+            task_name,
+            dataloaders,
+            bert_encoder,
+            BertRegressionHead(),
+            scorer,
+            loss_hat_func=loss_hat_func,
+            output_hat_func=torch.sigmoid,
+        )
+
     elif task_name == "QNLI":
         raise NotImplementedError
     elif task_name == "SNLI":
