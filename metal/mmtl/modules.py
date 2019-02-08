7f1a82ddba6e159190397379e0e34f9b4a988d56
Clara McCreery <chmccreery@gmail.com>
2019-02-07 18:35:00 -0800

    add task

---
+++ metal/mmtl/modules.py
@@ -0,0 +1,52 @@
+import os
+
+import torch.nn as nn
+from dataset import BERTDataset
+from pytorch_pretrained_bert import BertForMaskedLM, BertModel, BertTokenizer
+
+BERT_output_dim = 768
+
+
+class BertEncoder(nn.Module):
+    def __init__(self):
+        super(BertEncoder, self).__init__()
+        self.bert_model = BertModel.from_pretrained("bert-base-uncased")
+
+    def forward(self, data):
+        tokens, segments, mask = data
+        # TODO: check if we should return all layers or just last hidden representation
+        _, hidden_layer = self.bert_model(tokens, segments, mask)
+        return hidden_layer
+
+
+def createBertDataloader(
+    task_name,
+    batch_sz=8,
+    sent1_idx=0,
+    sent2_idx=-1,
+    label_idx=1,
+    header=1,
+    label_fn=lambda x: int(x) + 1,
+):
+    # model = 'bert-base-uncased' # also try bert-base-multilingual-cased (recommended)
+    src_path = os.path.join(os.environ["GLUEDATA"], task_name, "{}.tsv")
+    dataloaders = {}
+    for split in ["train", "dev"]:
+        dataset = BERTDataset(
+            src_path.format(split),
+            sent1_idx=sent1_idx,
+            sent2_idx=sent2_idx,
+            label_idx=label_idx,
+            skip_rows=header,
+            label_fn=label_fn,  # labels are scores [1, 2] (multiclass with cardinality k)
+        )
+        dataloaders[split] = dataset.get_dataloader(batch_size=batch_sz)
+    return [dataloaders["train"], dataloaders["dev"], None]
+
+
+def BertMulticlassHead(k):
+    return nn.linear([BERT_output_dim, k])
+
+
+def BertBinaryHead():
+    return BertMulticlassHead(2)
