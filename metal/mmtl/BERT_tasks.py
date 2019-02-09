cee50fd4bcf6015823aff3a0df052a44711a691a
Clara McCreery <chmccreery@gmail.com>
2019-02-08 16:14:12 -0800

    make dataloaders outside if statement

--- metal/mmtl/BERT_tasks.py
+++ metal/mmtl/BERT_tasks.py
@@ -20,18 +20,14 @@ from metal.mmtl.utils.metrics import pearson_corr, spearman_corr
 def create_task(task_name):
     bert_model = "bert-base-uncased"
     bert_encoder = BertEncoder(bert_model)
-
-    if task_name == "CoLA":
-        dataloaders = get_all_dataloaders(COLADataset, bert_model)
-
+    dataloaders = get_all_dataloaders(task_name, bert_model)
+    
+    if task_name == "COLA":
         def matthews_corr(targets, predictions):
-            import pdb
-
             pdb.set_trace()
             predictions = np.argmax(predictions, 1)
             matthews = matthews_corrcoef(targets, predictions)
             return {"matthews_corr": matthews}
-
         return Task(
             task_name,
             dataloaders,
@@ -44,27 +40,29 @@ def create_task(task_name):
             ),
         )
 
-    if task_name == "SST-2":
-        dataloaders = get_all_dataloaders(SST2Dataset, bert_model)
+    if task_name == "SST2":
         return Task(task_name, dataloaders, bert_encoder, BertBinaryHead())
 
     elif task_name == "MNLI":
         raise NotImplementedError
+
     elif task_name == "RTE":
         raise NotImplementedError
+
     elif task_name == "WNLI":
         raise NotImplementedError
+
     elif task_name == "QQP":
         raise NotImplementedError
+
     elif task_name == "MRPC":
         raise NotImplementedError
-    elif task_name == "STS-B":
-        dataloaders = get_all_dataloaders(STSBDataset, bert_model)
+
+    elif task_name == "STSB":
         scorer = Scorer(
             standard_metrics=["train/loss", "valid/loss"],
             custom_metric_fns=[pearson_corr, spearman_corr],
         )
-
         # x -> sigmoid -> [0,1], and compute mse_loss (y \in [0,1])
         loss_hat_func = lambda x, y: F.mse_loss(torch.sigmoid(x), y)
         return Task(
@@ -79,9 +77,12 @@ def create_task(task_name):
 
     elif task_name == "QNLI":
         raise NotImplementedError
+
     elif task_name == "SNLI":
         raise NotImplementedError
+
     elif task_name == "SciTail":
         raise NotImplementedError
+
     else:
         raise ValueError(f"{task_name} does not exist.")
