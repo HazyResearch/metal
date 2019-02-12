b210ad6aa54399061ec20bb6bd94bfcd4cbe1b9f
Vincent Chen <vincentschen@yahoo.com>
2019-02-11 17:51:46 -0800

    use tqdm jupyter for dataset loader

--- metal/mmtl/dataset.py
+++ metal/mmtl/dataset.py
@@ -6,7 +6,21 @@ import torch
 import torch.utils.data as data
 from pytorch_pretrained_bert import BertTokenizer
 from torch.utils.data.sampler import SubsetRandomSampler
-from tqdm import tqdm
+
+# Import tqdm_notebook if in Jupyter notebook
+try:
+    from IPython import get_ipython
+
+    if "IPKernelApp" not in get_ipython().config:
+        raise ImportError("console")
+except (AttributeError, ImportError):
+    from tqdm import tqdm
+else:
+    # Only use tqdm notebook if not in travis testing
+    if "CI" not in os.environ:
+        from tqdm import tqdm_notebook as tqdm
+    else:
+        from tqdm import tqdm
 
 
 def tsv_path_for_dataset(dataset_name, dataset_split):
