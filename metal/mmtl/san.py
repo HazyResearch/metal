08086181f7c0697bae22d863de42aa2134acbbe0
Sen Wu <senwu@cs.stanford.edu>
2019-02-14 04:32:22 -0800

    update san

--- metal/mmtl/san.py
+++ metal/mmtl/san.py
@@ -1,24 +1,66 @@
+import random
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.nn.parameter import Parameter
 
 from metal.mmtl.modules import BertEncoder
 
 
-class AverageLayer(nn.Module):
-    def __init__(self, k=5):
-        super(AverageLayer, self).__init__()
-        self.k = k
+class DropoutWrapper(nn.Module):
+    """
+    This is a dropout wrapper which supports the fix mask dropout
+    by: xiaodl
+    """
+
+    def __init__(self, dropout_p=0, enable_vbp=True):
+        super(DropoutWrapper, self).__init__()
+        """variational dropout means fix dropout mask
+        ref: https://discuss.pytorch.org/t/dropout-for-rnns/633/11
+        """
+        self.enable_variational_dropout = enable_vbp
+        self.dropout_p = dropout_p
 
     def forward(self, x):
-        return x / self.k
+        """
+            :param x: batch * len * input_size
+        """
+        if self.training is False or self.dropout_p == 0:
+            return x
+
+        if len(x.size()) == 3:
+            mask = Variable(
+                1.0
+                / (1 - self.dropout_p)
+                * torch.bernoulli(
+                    (1 - self.dropout_p)
+                    * (x.data.new(x.size(0), x.size(2)).zero_() + 1)
+                ),
+                requires_grad=False,
+            )
+            return mask.unsqueeze(1).expand_as(x) * x
+        else:
+            return F.dropout(x, p=self.dropout_p, training=self.training)
+
+
+class AverageLayer(nn.Module):
+    def __init__(self):
+        super(AverageLayer, self).__init__()
+
+    def forward(self, x):
+        return torch.mean(x, 2)
 
 
 class LinearSelfAttn(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_size, dropout=0.1):
         super(LinearSelfAttn, self).__init__()
         self.linear = nn.Linear(input_size, 1)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, x_mask):
+        x = self.dropout(x)
         scores = self.linear(x).view(x.size(0), x.size(1))
         scores.data.masked_fill_(x_mask.data, -float("inf"))
         alpha = torch.softmax(scores, 1)
@@ -26,11 +68,14 @@ class LinearSelfAttn(nn.Module):
 
 
 class BilinearSelfAttn(nn.Module):
-    def __init__(self, x_size, y_size):
+    def __init__(self, x_size, y_size, dropout=0.1):
         super(BilinearSelfAttn, self).__init__()
         self.linear = nn.Linear(y_size, x_size)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, y, x_mask):
+        x = self.dropout(x)
+        y = self.dropout(y)
         Wy = self.linear(y)
         xWy = x.bmm(Wy.unsqueeze(2)).squeeze(2)
         xWy.data.masked_fill_(x_mask.data, -float("inf"))
@@ -38,16 +83,34 @@ class BilinearSelfAttn(nn.Module):
         return beta.unsqueeze(1).bmm(x).squeeze(1)
 
 
+def generate_mask(new_data, dropout_p=0.0, is_training=False):
+    if not is_training:
+        dropout_p = 0.0
+    new_data = (1 - dropout_p) * (new_data.zero_() + 1)
+    for i in range(new_data.size(0)):
+        one = random.randint(0, new_data.size(1) - 1)
+        new_data[i][one] = 1
+    mask = Variable(
+        1.0 / (1 - dropout_p) * torch.bernoulli(new_data), requires_grad=False
+    )
+    return mask
+
+
 class SAN(nn.Module):
-    def __init__(self, bert_model, emb_size=100, hidden_size=100, num_classes=2, k=5):
+    def __init__(
+        self, bert_model, emb_size=100, hidden_size=100, num_classes=2, k=5, dropout=0.1
+    ):
         super(SAN, self).__init__()
         self.bert_model = bert_model
         self.sent1_attn = LinearSelfAttn(input_size=emb_size)
         self.sent2_attn = BilinearSelfAttn(emb_size, emb_size)
         self.final_linear = nn.Linear(emb_size * 4, num_classes)
-        self.rnn = nn.GRU(emb_size, hidden_size, 1, batch_first=True)
+        self.rnn = nn.GRUCell(emb_size, hidden_size)
         self.k = k
         self.num_classes = num_classes
+        self.dropout_num = dropout
+        self.dropout = nn.Dropout(dropout)
+        self.alpha = Parameter(torch.zeros(1, 1, 1))
 
     def forward(self, X):
         idx_matrix, seg_matrix, mask_matrix = X
@@ -56,20 +119,33 @@ class SAN(nn.Module):
 
         output_layer, _ = self.bert_model.forward(X)
 
-        res = output_layer.new_zeros((batch_size, self.num_classes))
+        res = []
+        #         res = output_layer.new_zeros((batch_size, self.num_classes))
 
         sk = self.sent1_attn(output_layer, (1 - mask_matrix + seg_matrix).byte())
 
         for i in range(self.k):
 
             xk = self.sent2_attn(output_layer, sk, (1 - seg_matrix).byte())
-            _, sk = self.rnn(xk.unsqueeze(1), sk.unsqueeze(0))
-            sk = sk.squeeze(0)
+            sk = self.dropout(sk)
+            sk = self.rnn(xk, sk)
+            f = self.final_linear(torch.cat((sk, xk, torch.abs(sk - xk), sk * xk), 1))
+            #             f = torch.softmax(
+            #                 self.final_linear(torch.cat((sk, xk, torch.abs(sk - xk), sk * xk), 1)),
+            #                 1,
+            #             )
+            res.append(f)
+        #             res += f
 
-            f = torch.softmax(
-                self.final_linear(torch.cat((sk, xk, torch.abs(sk - xk), sk * xk), 1)),
-                1,
-            )
-            res += f
-
-        return res
+        mask = generate_mask(
+            self.alpha.data.new(batch_size, self.k), self.dropout_num, self.training
+        )
+        mask = [m.contiguous() for m in torch.unbind(mask, 1)]
+        #         print(mask)
+        scores_list = [
+            mask[idx].view(batch_size, 1).expand_as(inp) * torch.softmax(inp, 1)
+            for idx, inp in enumerate(res)
+        ]
+        #         print(scores_list)
+        scores = torch.stack(scores_list, 2)
+        return scores
