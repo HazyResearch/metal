83b77cf42629aa1f122bcf64429837625d3a800b
Clara McCreery <chmccreery@gmail.com>
2019-02-19 08:10:50 -0800

    configs and shell script for running single tasks

---
+++ metal/mmtl/aws/launch_aws.sh
@@ -0,0 +1,17 @@
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/COLA_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/SST2_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/MNLI_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/RTE_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/WNLI_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/QQP_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/MRPC_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/STSB_dict.py --n_machines 1 --n_trials 1;
+
+python metal/mmtl/aws/mmtl_aws.py --mode run --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.2xlarge  --aws_access_key_id AKIAJA5UCLCUMXFVB4FQ --aws_secret_access_key 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 --configpath metal/mmtl/aws/single_task_configs/QNLI_dict.py --n_machines 1 --n_trials 1;
---
+++ metal/mmtl/aws/single_task_configs/SST2_dict.py
@@ -0,0 +1,45 @@
+# Arguments for launch script
+
+search_space = {
+    # hyperparams
+    "l2": [0.01],
+    "lr": {"range": [1e-5], "scale": "log"},
+    # Glue
+    "trainer_metrics": ["glue"],
+    "lr_scheduler": "linear",
+    "warmup_steps": 0.5,
+    "warmup_unit": "epochs",
+}
+
+launch_args = {
+    # Device args
+    "device": 0,
+    # Model specification
+    "bert_model": "bert-base-uncased",
+    "bert_output_dim": 768,
+    # Dataloader specification
+    "max_len": 200,
+    "lr_scheduler": "linear",
+    # Checkpointing and logging
+    "log_every": 0.25,
+    "score_every": 0.25,
+    "checkpoint_metric": "SST2/valid/accuracy",
+    "checkpoint_metric_mode": "max",
+    "checkpoint_clean": 1,
+    "checkpoint_best": 1,
+    "progress_bar": 1,
+    # Training settings
+    "lr_scheduler": "linear",
+    "lr": 1e-5,
+    "n_epochs": 5,
+    "l2": 0.01,
+    "batch_size": 32,
+    "split_prop": 0.8,
+    "max_datapoints": -1,
+    # Writer arguments
+    "writer": "tensorboard",
+    # "run_dir": "test_run",
+    # "run_name": "test_name"
+    # Task arguments
+    "tasks": "SST2",
+}
