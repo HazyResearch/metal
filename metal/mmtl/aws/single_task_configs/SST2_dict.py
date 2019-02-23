669dbcf68b2acf4fe950e3df9ae5a0f0cdee1723
Vincent Chen <vincentschen@yahoo.com>
2019-02-23 14:05:00 -0800

    Add latest SST2 and STSB HP configs

--- metal/mmtl/aws/single_task_configs/SST2_dict.py
+++ metal/mmtl/aws/single_task_configs/SST2_dict.py
@@ -2,11 +2,14 @@
 
 search_space = {
     # hyperparams
-    "l2": [0, 0.001, 0.01],
-    "lr": [1e-8, 1e-7, 1e-6, 5e-5, 1e-5],
-    "lr_scheduler": "linear",
-    "warmup_steps": 0.5,
-    "warmup_unit": "epochs",
+    #    "batch_size": [8, 16, 32],
+    #    "lr": {"range": [1e-8, 1e-4], "scale": "log"},
+    "gamma": [0.9, 0.99, 0.999, 0.9999, 0.99999],
+    "lr": [6.083425e-05],
+    "l2": [0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8],
+    "n_epochs": [5, 10]
+    #    "warmup_steps": 0.5,
+    #    "warmup_unit": "epochs",
 }
 
 launch_args = {
@@ -17,7 +20,6 @@ launch_args = {
     "bert_output_dim": 768,
     # Dataloader specification
     "max_len": 256,
-    "lr_scheduler": "linear",
     # Checkpointing and logging
     "log_every": 0.2,
     "score_every": 0.2,
@@ -27,10 +29,10 @@ launch_args = {
     "checkpoint_best": 1,
     "progress_bar": 1,
     # Training settings
-    "lr_scheduler": "linear",
+    "lr_scheduler": "exponential",
     "lr": 1e-5,
-    "n_epochs": 10,
-    "l2": 0.01,
+    "n_epochs": 5,
+    "l2": 0.0,
     "batch_size": 32,
     "max_datapoints": -1,
     # Writer arguments
