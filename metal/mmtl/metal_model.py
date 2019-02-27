f13c848188f501e723877ff70e4db16d89c1fabc
Braden Hancock <hancock.braden@gmail.com>
2019-02-26 16:12:55 -0800

    Make minor cosmetic changes

--- metal/mmtl/metal_model.py
+++ metal/mmtl/metal_model.py
@@ -97,6 +97,7 @@ class MetalModel(nn.Module):
 
     def calculate_loss(self, X, Y, task_names):
         """Returns a dict of {task_name: loss (an FloatTensor scalar)}."""
+
         return {
             t: self.loss_hat_funcs[t](out, move_to_device(Y, self.config["device"]))
             for t, out in self.forward(X, task_names).items()
