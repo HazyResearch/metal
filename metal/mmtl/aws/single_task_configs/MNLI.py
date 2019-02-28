89fd3408c057d5810b70f2d60be6737ea9f7bf74
Clara McCreery <chmccreery@gmail.com>
2019-02-27 19:03:48 -0800

    Update configs for training single task on aws.

--- metal/mmtl/aws/single_task_configs/MNLI.py
+++ metal/mmtl/aws/single_task_configs/MNLI.py
@@ -2,10 +2,10 @@
 
 search_space = {
     # hyperparams
-    "l2": [1e-1, 1e-2, 1e-3],  # 1e-4 looked same as 1e-3
-    "batch_size": [32],
-    "lr": [5e-5],  # 1e-4 and 1e-5 did worse
-    "min_lr": [1e-5, 1e-6, 0],
+    "l2": [1e-2],  # 1e-4 looked same as 1e-3
+    "batch_size": [16],
+    "lr": [8e-5, 5e-5, 2e-5, 1e-5],  # 1e-4 and 1e-5 did worse
+    # "min_lr": [1e-5, 1e-6, 0],
     "lr_scheduler": ["linear"],  # exponential did worse
 }
 
@@ -14,13 +14,13 @@ launch_args = {
     "device": 0,
     "fp16": 1,
     # Model specification
-    "bert_model": "bert-base-uncased",
+    "bert_model": "bert-large-uncased",
     # Dataloader specification
     "max_len": 200,
     # Checkpointing and logging
     "log_every": 0.1,
     "score_every": 0.1,
-    "checkpoint_metric": "MNLI/valid/accuracy",
+    "checkpoint_metric": "model/valid/glue",
     "checkpoint_metric_mode": "max",
     "checkpoint_clean": 1,
     "checkpoint_best": 1,
@@ -28,7 +28,7 @@ launch_args = {
     # Training settings
     # "lr_scheduler": "linear",
     "lr": 1e-5,
-    "n_epochs": 5,
+    "n_epochs": 10,
     "l2": 0.01,
     "batch_size": 32,
     "max_datapoints": -1,
