eb5892433de8c74ebfa387f0765a7b580b2c400b
Clara McCreery <chmccreery@gmail.com>
2019-02-27 19:07:16 -0800

    Add configs for more single tasks.

---
+++ metal/mmtl/aws/single_task_configs/SST2.py
@@ -0,0 +1,42 @@
+# Arguments for launch script
+
+search_space = {
+    # hyperparams
+    "l2": [0],
+    "batch_size": [32],
+    "lr": [6.08e-5],  # [1e-4,7e-5,6.08e-5,6.08e-5,5e-5],
+    "lr_scheduler": ["exponential"],
+    "gamma": [0.999],
+}
+
+launch_args = {
+    # Device args
+    # "device": 0,
+    "fp16": 1,
+    # Model specification
+    "bert_model": "bert-large-uncased",
+    # "bert_output_dim": 1024,
+    # Dataloader specification
+    "max_len": 200,
+    # Checkpointing and logging
+    "log_every": 0.1,
+    "score_every": 0.1,
+    "checkpoint_metric": "model/valid/glue",
+    "checkpoint_metric_mode": "max",
+    # "checkpoint_clean": 1,
+    "checkpoint_best": 1,
+    "progress_bar": 1,
+    # Training settings
+    # "lr_scheduler": "linear",
+    # "lr": 1e-5,
+    "n_epochs": 10,
+    # "l2": 0.01,
+    # "batch_size": 32,
+    # "max_datapoints": -1,
+    # Writer arguments
+    "writer": "tensorboard",
+    # "run_dir": "test_run",
+    # "run_name": "test_name"
+    # Task arguments
+    "tasks": "SST2",
+}
---
+++ metal/mmtl/aws/single_task_configs/STSB.py
@@ -0,0 +1,42 @@
+# Arguments for launch script
+
+search_space = {
+    # hyperparams
+    "l2": [0],
+    "batch_size": [2],
+    "lr": [1e-5, 8e-6, 8e-6, 6e-6],
+    "lr_scheduler": ["exponential"],
+    "gamma": [0.99],
+}
+
+launch_args = {
+    # Device args
+    "device": 0,
+    "fp16": 1,
+    # Model specification
+    "bert_model": "bert-large-uncased",
+    # "bert_output_dim": 1024,
+    # Dataloader specification
+    "max_len": 200,
+    # Checkpointing and logging
+    "log_every": 0.1,
+    "score_every": 0.1,
+    "checkpoint_metric": "model/valid/glue",
+    "checkpoint_metric_mode": "max",
+    "checkpoint_clean": 1,
+    "checkpoint_best": 1,
+    "progress_bar": 1,
+    # Training settings
+    # "lr_scheduler": "linear",
+    "lr": 1e-5,
+    "n_epochs": 10,
+    "l2": 0.01,
+    "batch_size": 32,
+    "max_datapoints": -1,
+    # Writer arguments
+    "writer": "tensorboard",
+    # "run_dir": "test_run",
+    # "run_name": "test_name"
+    # Task arguments
+    "tasks": "STSB",
+}
