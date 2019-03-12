815ec31726bc7ee3897aef1bbe1aeaa11dae5dd7
Clara McCreery <chmccreery@gmail.com>
2019-03-11 19:40:31 -0700

    Allow task argument to finetune.sh again.

--- metal/mmtl/aws/finetune.sh
+++ metal/mmtl/aws/finetune.sh
@@ -1,3 +1,4 @@
 MODE=${1:-launch_and_run}
+TASK=${2:-COLA}
 
-python metal/mmtl/aws/mmtl_aws.py --mode $MODE --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.8xlarge  --aws_access_key_id $AWS_ACCESS_KEY_ID --aws_secret_access_key $AWS_SECRET_ACCESS_KEY --configpath metal/mmtl/aws/single_task_configs/STSB.py --n_machines 1 --commit_hash e950f3b8e263db516e6928748c4c6de29c45a416 --ami ami-01b22f90823e1b2af --run_name test2
+python metal/mmtl/aws/mmtl_aws.py --mode $MODE --keypath /dfs/scratch0/maxlam/mmtl_share/mmtl_shared_east.pem.txt --instance_type p3.8xlarge  --aws_access_key_id $AWS_ACCESS_KEY_ID --aws_secret_access_key $AWS_SECRET_ACCESS_KEY --configpath metal/mmtl/aws/single_task_configs/$TASK.py --n_machines 5 --commit_hash ab66f42b207f6273e408c86d450e8374d65b5e85 --ami ami-01b22f90823e1b2af --run_name $TASK
