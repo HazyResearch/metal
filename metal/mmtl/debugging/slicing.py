f37a826cdb36c709a3c6321a70260aaf542b2bd7
Clara McCreery <chmccreery@gmail.com>
2019-03-11 23:45:55 -0700

    Add second slicing function and remove wrong QNLI data.

--- metal/mmtl/debugging/slicing.py
+++ metal/mmtl/debugging/slicing.py
@@ -1,8 +1,31 @@
 import pandas as pd
 
+from metal.mmtl.debugging.lf_helpers import regex_present
 
-def slice_odd_punc(row):
-    if "-" in row["sentence1"] or ";" in row["sentence1"] or ":" in row["sentence1"]:
+
+def slice_dashsemicolon(row):
+    if "-" in row["sentence1"] or ";" in row["sentence1"]:
         return True
     else:
         return False
+
+
+def slicing_function(row):
+    people = 0
+    for pronoun in ["she", "her", "hers"]:
+        if regex_present(row, pronoun, fields=["sentence1"]):
+            people += 1
+            continue
+    for pronoun in ["he", "him", "his"]:
+        if regex_present(row, pronoun, fields=["sentence1"]):
+            people += 1
+            continue
+    for pronoun in ["you", "your", "yours"]:
+        if regex_present(row, pronoun, fields=["sentence1"]):
+            people += 1
+            continue
+    for pronoun in ["I", "my", "me", "mine"]:
+        if regex_present(row, pronoun, fields=["sentence1"]):
+            people += 1
+            continue
+    return people > 3
