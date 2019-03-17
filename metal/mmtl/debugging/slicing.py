9887c40574ac786e131d11de735d2a6357b2388f
Clara McCreery <chmccreery@gmail.com>
2019-03-17 11:11:27 -0700

    Add slicing functions and updates to work with latest error_analysis.tsv files

--- metal/mmtl/debugging/slicing.py
+++ metal/mmtl/debugging/slicing.py
@@ -1,5 +1,77 @@
 import pandas as pd
+import spacy
 from nltk.translate.bleu_score import sentence_bleu
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def slice_who(row):
+    sent2 = row["sentence2"].replace(" ##", "")
+    sent2 = nlp(sent2)
+    for ent in sent2.ents:
+        if ent.label_ in ["PERSON"] and ent.text not in row["sentence1"]:
+            return True  # event, person, org, gpe in question but not named in answer
+    return False
+
+
+def slice_entsecondonly(row):
+    sent2 = nlp(row["sentence2"])
+    for ent in sent2.ents:
+        if ent.text not in row["sentence1"]:
+            return True
+    return False
+
+
+def slice_function(row):
+    sent1 = row["sentence1"].split()
+    sent2 = row["sentence2"].split()
+    word = "why"
+    if word in sent1 and word not in sent2:
+        return True
+    if word in sent2 and word not in sent1:
+        return True
+    return False
+
+
+def slice_numbers(row):
+    sent1 = row["sentence1"]
+    sent2 = row["sentence2"]
+    for num in range(10):
+        if str(num) in sent2 and str(num) not in sent1:
+            return True
+        if str(num) in sent1 and str(num) not in sent2:
+            return True
+    return False
+
+
+def slice_firstsecondperson(row):
+    sent1 = row["sentence1"].split()
+    for word in ["you", "your", "yours", "we", "us", "our", "I", "my", "me"]:
+        if word in sent1:
+            return True
+    return False
+
+
+def slice_contraction(row):
+    sent1 = row["sentence1"].split()
+    if "'" in sent1:
+        return True
+    return False
+
+
+def slice_qmark(row):
+    sent1 = row["sentence1"].split()
+    if sent1[-1] == "?":
+        return True
+    return False
+
+
+def slice_qword(row):
+    sent1 = row["sentence1"].split()
+    for word in ["who", "what", "where", "when", "why", "how"]:
+        if word in sent1 and "?" not in sent1:
+            return True
+    return False
 
 
 def slice_quotes(row):
@@ -14,7 +86,7 @@ def slice_quotes(row):
 def slice_modals(row):
     sent1 = row["sentence1"].split()
     if any(
-        [word in sent1 for word in ["could", "would"]]
+        [word in sent1 for word in ["could", "would", "should", "ought"]]
     ):  # could train on 'should' too
         return True
     return False
@@ -23,7 +95,7 @@ def slice_modals(row):
 def slice_qualifier(row):
     sent1 = row["sentence1"].split()
     if any(
-        [word in sent1 for word in ["while", "although", "aside"]]
+        [word in sent1 for word in ["while", "although", "aside", "but"]]
     ):  # could train on 'but' too
         return True
     return False
