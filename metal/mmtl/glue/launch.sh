1aa6e6522c162faf2eb5b3869f8d0f2fc5653cd3
Clara McCreery <chmccreery@gmail.com>
2019-04-08 13:34:01 -0700

    Add support for half precision and flag for finetuning to launch.sh script

--- metal/mmtl/glue/launch.sh
+++ metal/mmtl/glue/launch.sh
@@ -1,6 +1,11 @@
 # launches single task or multitask models of MetalModel with favorable GLUE parameters
+# declare -a arr=("COLA" "SST2" "STSB" "MNLI" "MRPC" "QNLI" "QQP" "RTE")
+
+# for task in "${arr[@]}"
+# do
 python launch.py \
     --device 0 \
+    --fp16 1 \
     --bert_model bert-base-uncased \
     --max_len 200 \
     --warmup_unit "epochs" \
@@ -12,11 +17,12 @@ python launch.py \
     --checkpoint_metric_mode max \
     --checkpoint_best True \
     --progress_bar 1 \
-    --lr 5e-3 \
+    --lr 5e-5 \
     --l2 0 \
     --batch_size 32 \
-    --tasks SST2 \
+    --tasks STSB \
     --split_prop None \
-    --n_epochs 10 \
-    --max_datapoints -1 \
-    --reinit_bert True \
+    --n_epochs 3 \
+    --max_datapoints 1000 \
+    --model_weights /dfs/scratch0/mccreery/repos/metal/metal/mmtl/aws/output/2019_04_04_15_29_09/1/logdir/2019_04_05/QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA_19_58_22/best_model.pth
+# done
