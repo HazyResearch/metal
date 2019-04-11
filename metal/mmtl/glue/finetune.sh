9fce1a82ffdd4b8cce9858d0109af6285670a85b
Clara McCreery <chmccreery@gmail.com>
2019-04-10 18:01:47 -0700

    Create scripts for doing multitask training, multitask finetuning with frozen bert, and single task finetune.

--- metal/mmtl/glue/finetune.sh
+++ metal/mmtl/glue/finetune.sh
@@ -1,31 +1,29 @@
 # launches single task or multitask models of MetalModel with favorable GLUE parameters
-#declare -a arr=("COLA" "SST2" "RTE" "MRPC" "MNLI" "QNLI" "QQP")
+declare -a arr=("COLA" "SST2" "RTE" "MRPC" "MNLI" "QNLI" "QQP")
 
-# for task in "${arr[@]}"
-# do
+for task in "${arr[@]}"
+do
 python launch.py \
-    --seed 891941 \
     --device 0 \
-    --fp16 0 \
+    --fp16 1 \
     --bert_model bert-base-uncased \
     --max_len 200 \
     --warmup_unit "epochs" \
     --lr_scheduler "linear" \
     --min_lr 0 \
-    --log_every 0.1 --score_every 0.1 \
+    --log_every 0.25 --score_every 0.25 \
     --checkpoint_dir checkpoints \
     --checkpoint_metric model/valid/gold/glue_score \
     --checkpoint_metric_mode max \
     --checkpoint_best True \
-    --checkpoint_tasks True \
     --progress_bar 1 \
     --attention 1 \
     --lr 5e-5 \
     --l2 0 \
     --batch_size 16 \
-    --tasks QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA \
+    --tasks $task \
     --split_prop None \
-    --n_epochs 10 \
+    --n_epochs 5 \
     --max_datapoints -1 \
-#    --model_weights /dfs/scratch0/mccreery/repos/metal/metal/mmtl/aws/output/2019_04_08_17_04_03/0/logdir/2019_04_09/QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA_00_21_20/best_model.pth
-# done
+    --model_weights /dfs/scratch0/mccreery/repos/metal/metal/mmtl/aws/output/2019_04_08_17_04_03/0/logdir/2019_04_09/QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA_00_21_20/best_model.pth
+done
--- metal/mmtl/glue/finetune_freeze.sh
+++ metal/mmtl/glue/finetune_freeze.sh
@@ -1,10 +1,9 @@
 # launches single task or multitask models of MetalModel with favorable GLUE parameters
-#declare -a arr=("COLA" "SST2" "RTE" "MRPC" "MNLI" "QNLI" "QQP")
+# declare -a arr=("COLA" "SST2" "RTE" "MRPC" "MNLI" "QNLI" "QQP")
 
 # for task in "${arr[@]}"
 # do
 python launch.py \
-    --seed 891941 \
     --device 0 \
     --fp16 0 \
     --bert_model bert-base-uncased \
@@ -12,20 +11,20 @@ python launch.py \
     --warmup_unit "epochs" \
     --lr_scheduler "linear" \
     --min_lr 0 \
-    --log_every 0.1 --score_every 0.1 \
+    --log_every 0.25 --score_every 0.25 \
     --checkpoint_dir checkpoints \
     --checkpoint_metric model/valid/gold/glue_score \
     --checkpoint_metric_mode max \
     --checkpoint_best True \
-    --checkpoint_tasks True \
     --progress_bar 1 \
     --attention 1 \
+    --freeze_bert 1 \
     --lr 5e-5 \
     --l2 0 \
     --batch_size 16 \
     --tasks QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA \
     --split_prop None \
-    --n_epochs 10 \
+    --n_epochs 5 \
     --max_datapoints -1 \
-#    --model_weights /dfs/scratch0/mccreery/repos/metal/metal/mmtl/aws/output/2019_04_08_17_04_03/0/logdir/2019_04_09/QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA_00_21_20/best_model.pth
+    --model_weights /dfs/scratch0/mccreery/repos/metal/metal/mmtl/aws/output/2019_04_08_17_04_03/0/logdir/2019_04_09/QNLI,STSB,MRPC,QQP,WNLI,RTE,MNLI,SST2,COLA_00_21_20/best_model.pth
 # done
